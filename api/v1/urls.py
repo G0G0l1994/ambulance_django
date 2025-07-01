ded66a03317626e13f164a8ed57727c8ae668ff3
Vasily Oleinikov <vasia94.94@gmail.com>
2025-07-01 14:55:59 +0300

    users api with class

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from api.v1.views.user import user_create, users_list
+from api.v1.views.user import ReactView
 
-urlpatterns = [path("users/", users_list), path("users/create/", user_create)]
+urlpatterns = [path("users/", ReactView.as_view())]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/v1/views/user.py
+++ api/v1/views/user.py
@@ -1,37 +1,39 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.contrib.auth.models import User
 
-from api.v1.serializers.user import UserCreateSerializer, UserSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
+
+from api.v1.serializers.user import UserCreateSerializer
 from user.models import Profile
 
 
-@api_view(["GET", "POST"])
-def users_list(request, _format=None):
-    if request.method == "GET":
-        users = Profile.objects.select_related("user").all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class ReactView(APIView):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    parser_classes = [FormParser, MultiPartParser, JSONParser]
+    serializer_class = UserCreateSerializer
+    
+    
+    def get(self,request, format = None):
+        output = [
+            {
+                        "username": profile.user.username,
+                        "email": profile.user.email,
+                        "first_name": profile.user.first_name,
+                        "surname": profile.surname,
+                        "last_name": profile.user.last_name,
+                        "role": profile.role,
+                    }
+                    for profile in Profile.objects.select_related('user').all()
+        ]
 
+        return Response(output)
 
-@api_view(["GET", "POST"])
-def user_create(request, _format=None):
-    if request.method == "POST":
-        seriailizer = UserCreateSerializer(data=request.data)
-        if seriailizer.is_valid():
-            user = seriailizer.save()
-            return Response(
-                {
-                    "status": "success",
-                    "data": {
-                        "username": user.username,
-                        "email": user.email,
-                        "first_name": user.first_name,
-                        "surname": user.profile.surname,
-                        "last_name": user.last_name,
-                        "role": user.profile.role,
-                    },
-                },
-                status=status.HTTP_201_CREATED,
-            )
-        return Response(seriailizer.data, status=status.HTTP_400_BAD_REQUEST)
+    def post(self,request, format = None):
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
